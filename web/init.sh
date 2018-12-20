197d72657376bc826f146d69559957641cc4b67b
crool1002 <crool1002@icloud.com>
2018-12-21 00:44:11 +0300

    1

--- web/init.sh
+++ web/init.sh
@@ -1,5 +1,9 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+cp /home/box/stepic_web_project/ /home/box/
+sudo ﻿cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ls -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
+cd /home/box/web/hello/
+gunicorn -d 0.0.0.0:8080 hello:app

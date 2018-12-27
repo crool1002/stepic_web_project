8a0a0d1bed466c5b4a01594d6ac2b9f693aaf1c2
crool1002 <crool100@icloud.com>
2018-12-27 20:53:30 +0300

    1

--- web/hello/hello.py
+++ web/hello/hello.py
@@ -3,6 +3,7 @@ def app (environ, start_response):
         status = '200 OK'                                                       
         response_headers = [('Content-type','text/plain')]                      
         start_response(status, response_headers)                                
-        resp = environ['QUERY_STRING'].split("&")                               
-        resp = [bytes(item+"\r\n",'ascii') for item in resp]                    
+        resp = environ['QUERY_STRING'].split("&")
+        print(resp)
+        resp = [item+'\r\n' for item in resp]                    
         return resp

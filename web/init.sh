1afe47e2e7e54f9e5cdfc1bdbc6211e49dccdee2
crool1002 <crool100@icloud.com>
2018-12-27 23:45:30 +0300

    1

--- web/init.sh
+++ web/init.sh
@@ -1,10 +1,16 @@
-
 #sudo nano /etc/nginx/sites-enabled/nginx.conf
 #cp /home/parallels/stepic_web_project/web/etc/nginx.conf /etc/nginx/sites-enabled/
 #sudo unlink /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
-cd /home/parallels/stepic_web_project/web/ask/
+#cd /home/parallels/stepic_web_project/web/ask/
+#gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
+#cd /home/parallels/stepic_web_project/web/hello/
+#gunicorn -b 0.0.0.0:8080 hello:app &
+sudo nano /etc/nginx/sites-enabled/nginx.conf
+cp /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
+sudo unlink /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+cd /home/box/web/ask/
 gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
-cd /home/parallels/stepic_web_project/web/hello/
+cd /home/box/web/hello/
 gunicorn -b 0.0.0.0:8080 hello:app &
-
